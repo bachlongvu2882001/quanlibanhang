f879e7e469c7915e08a897002620a9d7f9cddac7
bachlongvu2882001 <66907506+bachlongvu2882001@users.noreply.github.com>
2021-11-15 11:15:33 +0700

    update

--- QLBH/main.cpp
+++ QLBH/main.cpp
@@ -3,164 +3,143 @@
 #include<string>
 #include <vector>
 using namespace std;
+//**************TAO CLASS SINH VIEN*****************
+class SV{  
+    protected:
+		string hoten;
+		string namsinh;
+		string gioitinh;
+		string lop;
+		int khoa;
+		int msv;
+		bool loaiSV;
+	public:
+		virtual void Nhap(); 
+		virtual void Xuat(); 
+};
+//Nhap cho class sinh vien
+void SV::Nhap()
+ {
+ 	cout<<"\nMa sinh vien: ";
+ 	cin>>msv;
+	cin.ignore();
+ 	cout<<"\nHo Va Ten: ";
+ 	getline(cin,hoten);
+ 	cout<<"\nNgay Thang Nam Sinh: ";
+ 	getline(cin,namsinh);
+ 	cout<<"\nGioi tinh: ";
+ 	getline(cin,gioitinh);
+ 	
+ }
+ //Xuat cho class sinh vien
+ void SV::Xuat()
+{
+    cout<<"\nMa sinh vien: "<<msv;
+ 	cout<<"\nHo Va Ten:  "<<hoten;
+ 	cout<<"\nNgay Thang Nam Sinh:  "<<namsinh;
+ 	cout<<"\nGioi tinh: "<<gioitinh;
+}
+//**************TAO CLASS MON HOC**************
+class Mon{
+	protected:
+	  string to_mmh;
+	  string l_mmh;
+	  string h_mmh;
+	  string td_mmh;
+	  float toan,li,hoa,td;
+	   int to_tclt,l_tclt,h_tclt,td_tclt;
+	   int to_tcth,l_tcth,h_tcth,td_tcth;
+       
+	public:
+	    void Nhap();
+	    void Xuat(); 
+};
+//Nhap cho class mon hoc
+void Mon::Nhap()
+{
+	cout<<"\nNhap diem toan = ";
+	cin>>toan;
+	cout<<"\nNhap diem li = ";
+	cin>>li;
+    cout<<"\nNhap diem hoa = ";
+	cin>>hoa;
+	cout<<"\nNhap diem thi the duc= ";
+	cin>>td;
+}
+//Xuat cho class mon hoc
+void Mon::Xuat(){
+    cout<<"\nMa mon hoc_TOAN: "<<to_mmh;
+	cout<<"\nDiem toan=  "<<toan<<" Tin chi li thuyet: "<<to_tclt<<" Tin chi thuc hanh: "<<to_tcth;
+	cout<<"\nMa mon hoc_LI: "<<l_mmh;
+	cout<<"\nDiem li=  "<<li<<" Tin chi li thuyet: "<<l_tclt<<" Tin chi thuc hanh: "<<l_tcth;
+	cout<<"\nMa mon hoc_HOA=  "<<h_mmh;
+	cout<<"\nDiem hoa=  "<<hoa<<" Tin chi li thuyet: "<<h_tclt<<" Tin chi thuc hanh: "<<h_tcth;
+	cout<<"\nMa mon hoc_THE_DUC=  "<<td_mmh;
+	cout<<"\nDiem the duc =  "<<td<<" Tin chi li thuyet: "<<td_tclt<<" Tin chi thuc hanh: "<<td_tcth;
+}
+//************TAO CLASS LOP CHINH QUY************
+class LopCQ:public Mon,public SV{ 
+	protected:
+    	string MaLop;
+    	string TenLop;
+    	int Khoa;
+    public:
+		void Nhap();
+        void Xuat();
+};
+//Nhap cho lop chinh quy
+void LopCQ::Nhap(){
+    cout<<"\nKhoa: ";
+    cin>>Khoa;
+    SV::Nhap();
+    Mon::Nhap();
+}
+//Xuat cho lop chinh quy
+void LopCQ::Xuat(){
+    cout<<"\nMa Lop: "<< MaLop << endl <<"Ten Lop: "<<TenLop<<endl<<"Khoa: "<<Khoa<<endl;
+    SV::Xuat();
+    Mon::Xuat();
+	//cout<<"\nDiem trung binh cua ban la:  "<<DTB();
+}
+//**********TAO CLASS LOP VUA HOC VUA LAM************
+//***************************************************
+//***************************************************
+class LopVHVL:public Mon,public SV{ 
+	private:
+    	string MaLop;
+    	string TenLop;
+    	int Khoa;
 
-//************CLASS SAN PHAM********************************
-//**********************************************************
-class SanPham{
-    protected:
-        string tensp,masp,mancc,tenncc;
-        int sl;
-        float dg;
-    public:
-         virtual void Nhap()=0;
-         virtual void Xuat()=0;
+	public:
+		void Nhap();
+		void Xuat();
 };
-//Nhap cho san pham
-void SanPham::Nhap(){
-    cout<<"\nMa san pham: ";
- 	cin>>masp;
-	cin.ignore();
- 	cout<<"\nTen san pham: ";
- 	getline(cin,tensp);
-    cout<<"\nMa nha cung cap: ";
- 	cin>>mancc;
- 	cout<<"\nTen nha cung cap: ";
- 	getline(cin,tenncc);
- 	cout<<"\nSo luong: ";
- 	cin>>sl;
-    cout<<"\nDon gia: ";
- 	cin>>dg;
+//Nhap cho lop vua hoc vua lam
+void LopVHVL::Nhap(){
+	cout<<"\nKhoa: ";
+    cin>>Khoa;
+    SV::Nhap();
+    Mon::Nhap();
 }
-//Xuat San san pham
-void SanPham::Xuat(){
-    cout<<"\nMa san pham: "<<masp;
- 	cout<<"\nTen san pham:  "<<tensp;
-    cout<<"\nMa nha cung cap: "<<mancc;
- 	cout<<"\nTen nha cung cap:  "<<tenncc;
- 	cout<<"\nSo luong:  "<<sl;
- 	cout<<"\nDon gia: "<<dg;
+//Xuat cho lop vua hoc vua lam
+void LopVHVL::Xuat(){
+	cout<<"\nMa Lop: "<< MaLop << endl <<"Ten Lop: "<<TenLop<<endl<<"Khoa: "<<Khoa<<endl;
+    SV::Xuat();
+    Mon::Xuat();
+	//cout<<"\nDiem trung binh cua ban la:  "<<DTB();
 }
-//************CLASS KHACH HANG********************************
-//**********************************************************
-class KhachHang{
-    protected:
-        string makh,tenkh,sdt,dc;
-    public:
-        KhachHang(string makh,string tenkh,string sdt,string dc);
-        virtual void Nhap()=0;
-         virtual void Xuat()=0;
-};
-//Ham tao cho khach hang 
-    KhachHang::KhachHang(string makh,string tenkh,string sdt,string dc){
-    this->makh = " ";
-    this->tenkh = " ";
-    this->sdt = " ";
-    this->dc = " ";
-}
-//Nhap cho khach hang
-void KhachHang::Nhap(){
-    cout<<"\nMa khach hang: ";
- 	cin>>makh;
-	cin.ignore();
- 	cout<<"\nTen khach hang: ";
- 	getline(cin,tenkh);
-    cout<<"\nSo dien thoai: ";
- 	cin>>sdt;
- 	cout<<"\nDia chi: ";
- 	cin>>dc;
-}
-//Xuat cho khach hang
-void KhachHang::Xuat(){
-    cout<<"\nMa khach hang: "<<makh;
- 	cout<<"\nTen khach hang:  "<<tenkh;
-    cout<<"\nSo dien thoai: "<<sdt;
- 	cout<<"\nDia chi:  "<<dc;
-}
-//************CLASS NHAN VIEN********************************
-//**********************************************************
-class NhanVien{
-    protected:
-        string manv,tennv,sdt,dc;
-    public:
-        virtual void Nhap()=0;
-        virtual void Xuat()=0; 
-};
-//Nhap cho khach hang
-void NhanVien::Nhap(){
-    cout<<"\nMa nhan vien: ";
- 	cin>>manv;
-	cin.ignore();
- 	cout<<"\nTen nhan vien: ";
- 	getline(cin,tennv);
-    cout<<"\nSo dien thoai: ";
- 	cin>>sdt;
- 	cout<<"\nDia chi: ";
- 	cin>>dc;
-}
-//Xuat cho khach hang
-void NhanVien::Xuat(){
-    cout<<"\nMa nhan vien: "<<manv;
- 	cout<<"\nTen nhan vien: "<<tennv;
-    cout<<"\nSo dien thoai: "<<sdt;
- 	cout<<"\nDia chi:  "<<dc;
-}
-//*************CLASS HOA DON********************************
-//**********************************************************
-class HoaDon:public SanPham,public KhachHang,public NhanVien{
-    private:
-        string mahd;
+//***********TAO CLASS KE THUA**********************
+//**************************************************
+//**************************************************
+class TongHop:public LopCQ,public LopVHVL{
+	protected:
+			vector <SV *> TH;
     public:
         void Nhap();
-        void Xuat(); 
+	    void Xuat();
 };
-//Nhap cho hoa don
-void HoaDon::Nhap()
-{
-    bool temp=false;
-    bool i=true;
-    cout<<"\nMa hoa don: ";
- 	cin>>mahd;
-     cout<<"\nMa nhan vien ban hang: ";
- 	cin>>manv;
-	cin.ignore();
- 	cout<<"\nTen nhan vien ban hang: ";
- 	getline(cin,tennv);
-	cin.ignore();
-    cout<<"\nMa khach hang: ";
- 	cin>>manv;
-	cin.ignore();
- 	cout<<"\nTen khach hang: ";
- 	getline(cin,tennv);
-     for(;i=temp;){
-            cout<<"\nMa san pham: ";
-            cin>>manv;
-            cout<<"\nTen san pham: ";
-            getline(cin,tensp);
-            cout<<"1-Tiep tuc them san pham"<<endl;
-            cout<<"2-Dung"<<endl;
-            int luachon;
-            cin>>luachon;
-            switch(luachon){
-                case 1: break;
-                case 2: temp=true; break;                 
-            }
-     }
-}
-//Xuat cho hoa don
-void HoaDon::Xuat(){
-    cout<<"\nMa hoa don: "<<mahd;
- 	cout<<"\nMa khach hang: "<<makh;
-    cout<<"\nTen khach hang: "<<tenkh;
- 	for(;;){
-         cout<<"Ma san pham: "<<masp<<endl;
-         cout<<"Ten san pham: "<<tensp<<endl;
-     }
-}
-//**********************MAIN********************************
-//**********************************************************
 int main()
 {
-   cout<<"hello world"<<endl;
     system("pause");
     return 0;
 }
